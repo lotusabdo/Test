2f7b6ce391824c0cbd07ead3b97566b7d7c72f68
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-23 00:49:13 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["CRAZ_UP", "II_U_6"]
+OWNER_NAME = "𝗯𝗮𝘀𝗵𝗮"
+BOT_TOKEN = "7387291189:AAFprQcvcNzsuQH0Q6U-JsaR_M6Wa-ZJlHk"
+DATABASE = "mongodb+srv://meshoe:meshoer@cluster0.uixyayo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+CHANNEL = "https://t.me/KK_DX"
+GROUP = "https://t.me/HHYTT"
+VIDEO = "https://t.me/KA_XB/2461"
+LOGS = "EGYPT_Soupr"
+
