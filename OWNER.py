c5fbbfec81fdcd588314912c0ebf5a232d93eecf
Tm_Boda <157813008+lotusabdo@users.noreply.github.com>
2024-06-23 01:04:23 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -2,7 +2,7 @@
 OWNER = ["CRAZ_UP", "II_U_6"]
 OWNER_NAME = "𝗯𝗮𝘀𝗵𝗮"
 BOT_TOKEN = "7387291189:AAFprQcvcNzsuQH0Q6U-JsaR_M6Wa-ZJlHk"
-DATABASE = "mongodb+srv://meshoe:meshoer@cluster0.uixyayo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DATABASE = "mongodb+srv://meshoe3:meshoe3@cluster0.bvaskli.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/KK_DX"
 GROUP = "https://t.me/HHYTT"
 VIDEO = "https://t.me/KA_XB/2461"
